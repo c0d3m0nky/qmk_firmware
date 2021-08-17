3b4ed221d8b1af8c3aa7c44e9eaf277d7218d5cd
Alberto Bec <alberto.bec@microfocus.com>
2021-08-17 17:16:04 -0400

    Small layout tweaks

--- keyboards/xbows/knight_plus/keymaps/default/keymap.c
+++ keyboards/xbows/knight_plus/keymaps/default/keymap.c
@@ -17,7 +17,7 @@
 #include "xbowscommon.h"
 
 enum {
-    M_LAMBDA = SAFE_RANGE
+	M_LAMBDA = SAFE_RANGE
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -37,30 +37,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |Ctrl | GUI |     Alter   |    Space   |   Ctrl   |   Shift   |     Space     |    Alter   |  FN  | Ctrl | Lft  |  Dn |  Rig |
    * |---------------------------------------------------------------------------------------------------------------------------------|
    */
-    [0] = LAYOUT(
-            KC_ESC,     KC_F13,     KC_F2,      KC_F3,  KC_F4,    KC_F5,      KC_F6,      KC_F7,    KC_F8,  KC_F9,      KC_F10,   KC_F11,     KC_F12,     KC_DEL,     KC_INS,
-            KC_GRV,     KC_1,       KC_2,       KC_3,   KC_4,     KC_5,                   KC_6,     KC_7,   KC_8,       KC_9,     KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,
-            KC_TAB,     KC_Q,       KC_W,       KC_E,   KC_R,     KC_T,       KC_Y,       KC_U,     KC_I,   KC_O,       KC_P,     KC_LBRC,    KC_RBRC,    KC_BSLS,    KC_HOME,
-            KC_LSFT,    KC_A,       KC_S,       KC_D,   KC_F,     KC_G,       KC_NO,      KC_H,     KC_J,   KC_K,       KC_L,     KC_SCLN,    KC_QUOT,    KC_ENT,     KC_END,
-            KC_LSFT,    KC_Z,       KC_X,       KC_C,   KC_V,     KC_B,       KC_NO,      KC_N,     KC_M,   KC_COMM,    KC_DOT,   KC_SLSH,    KC_RSFT,                KC_UP,
-            KC_LCTL,    KC_LGUI,    KC_LALT,            KC_SPC,   KC_LCTL,    KC_LSFT,    KC_SPC,           KC_RALT,              MO(1),      KC_RCTL,    KC_LEFT,    KC_DOWN,    KC_RGHT
-    ),
-    [1] = LAYOUT(
-            KC_NO,      KC_MPLY,    KC_VOLD,    KC_VOLU,    KC_MUTE,    KC_F17,   KC_F18,   KC_F19,   KC_F20,   KC_F21,     KC_F22,     KC_F23,   KC_F24,   KC_NO,      KC_PSCR,
-            KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,              KC_NO,    KC_NO,    KC_NO,      KC_NO,      KC_NO,    KC_NO,    KC_NO,      KC_NO,
-            KC_NO,      KC_NO,      RGB_VAI,    KC_NO,      KC_NO,      KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,      KC_NO,    KC_NO,    KC_NO,      KC_PGUP,
-            KC_CAPS,    KC_NO,      RGB_VAD,    KC_NO,      KC_NO,      KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,      KC_NO,    KC_NO,    KC_NO,      KC_PGDN,
-            KC_NO,      KC_NO,      KC_CALC,    KC_NO,      KC_NO,      KC_NO,    MO(2),    KC_NO,    KC_NO,    KC_NO,      M_LAMBDA,   KC_NO,    KC_NO,                KC_NO,
-            KC_NO,      KC_NO,      KC_NO,                  KC_NO,      KC_NO,    KC_NO,    KC_NO,              KC_MENU,                MO(1),    KC_NO,    KC_WBAK,    KC_NO,      KC_WFWD
-    ),
-    [2] = LAYOUT(
-            RESET,  EEP_RST,    KC_NO,  KC_NO,  KC_NO,  DEBUG,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-            KC_NO,  KC_NO,      KC_NO,  KC_NO,  KC_NO,  KC_NO,          KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-            KC_NO,  KC_NO,      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-            KC_NO,  KC_NO,      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-            KC_NO,  KC_NO,      KC_NO,  KC_NO,  KC_NO,  KC_NO,  MO(2),  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,          KC_NO,
-            KC_NO,  KC_NO,      KC_NO,          KC_NO,  KC_NO,  KC_NO,  KC_NO,          KC_NO,          MO(1),  KC_NO,  KC_NO,  KC_NO,  KC_NO
-    )
+	[0] = LAYOUT(
+			KC_ESC,     KC_F13,     KC_F2,      KC_F3,  KC_F4,    KC_F5,      KC_F6,      KC_F7,    KC_F8,  KC_F9,      KC_F10,   KC_F11,     KC_F12,     KC_DEL,     KC_INS,
+			KC_GRV,     KC_1,       KC_2,       KC_3,   KC_4,     KC_5,                   KC_6,     KC_7,   KC_8,       KC_9,     KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,
+			KC_TAB,     KC_Q,       KC_W,       KC_E,   KC_R,     KC_T,       KC_Y,       KC_U,     KC_I,   KC_O,       KC_P,     KC_LBRC,    KC_RBRC,    KC_BSLS,    KC_HOME,
+			KC_LSFT,    KC_A,       KC_S,       KC_D,   KC_F,     KC_G,       KC_NO,      KC_H,     KC_J,   KC_K,       KC_L,     KC_SCLN,    KC_QUOT,    KC_ENT,     KC_END,
+			KC_LSFT,    KC_Z,       KC_X,       KC_C,   KC_V,     KC_B,       KC_NO,      KC_N,     KC_M,   KC_COMM,    KC_DOT,   KC_SLSH,    KC_RSFT,                KC_UP,
+			KC_LCTL,    KC_LGUI,    KC_LALT,            KC_SPC,   KC_LCTL,    KC_LSFT,    KC_SPC,           KC_RALT,              MO(1),      KC_RCTL,    KC_LEFT,    KC_DOWN,    KC_RGHT
+	),
+	[1] = LAYOUT(
+			KC_NO,      KC_MPLY,    KC_VOLD,    KC_VOLU,    KC_MUTE,    KC_F17,   KC_F18,   KC_F19,   KC_F20,   KC_F21,     KC_F22,   KC_F23,   KC_F24,   KC_NO,      KC_PSCR,
+			KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,              KC_NO,    KC_NO,    KC_NO,      KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,
+			KC_NO,      KC_NO,      RGB_VAI,    KC_NO,      KC_NO,      KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_PGUP,
+			KC_CAPS,    KC_NO,      RGB_VAD,    KC_NO,      KC_NO,      KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_PGDN,
+			KC_NO,      KC_NO,      KC_CALC,    KC_NO,      KC_NO,      KC_NO,    MO(2),    KC_NO,    KC_NO,    KC_NO,      M_LAMBDA, KC_NO,    KC_NO,                KC_NO,
+			KC_NO,      KC_NO,      KC_NO,                  KC_NO,      KC_NO,    KC_NO,    KC_NO,              KC_MENU,              MO(1),    KC_NO,    KC_WBAK,    KC_NO,      KC_WFWD
+	),
+	[2] = LAYOUT(
+			RESET,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  EEP_RST,    KC_NO,  KC_NO,  KC_NO,  DEBUG,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+			KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,              KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+			KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+			KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+			KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      MO(2),  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,          KC_NO,
+			KC_NO,  KC_NO,  KC_NO,          KC_NO,  KC_NO,      KC_NO,  KC_NO,          KC_NO,          MO(1),  KC_NO,  KC_NO,  KC_NO,  KC_NO
+	)
 
 };
 
@@ -76,30 +76,30 @@ int _keyindices[][MATRIX_COLS] = {
 
 
 const KeyColor PROGMEM _rgblayers[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = RGBLAYOUT(
-            KRGB_DEF,       KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEV2,  KRGB_DEF, KRGB_DEF, KRGB_DEF,   KRGB_DEV1,  KRGB_DEV2,  KRGB_DEV2,  KRGB_DEF,     KRGB_DEF, KRGB_DEF,
-            KRGB_DEF,       KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEF,             KRGB_DEF, KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEF,     KRGB_DEF, KRGB_DEF,
-            KRGB_DEF,       KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEF, KRGB_DEF, KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEF,     KRGB_DEF, KRGB_MISNAMED,
-            KRGB_MISNAMED,  KRGB_HOME,  KRGB_HOME,  KRGB_HOME,  KRGB_HOME,  KRGB_DEF,   KRGB_OFF, KRGB_DEF, KRGB_HOME,  KRGB_HOME,  KRGB_HOME,  KRGB_HOME,  KRGB_DEF,     KRGB_DEF, KRGB_MISNAMED,
-            KRGB_DEF,       KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_OFF, KRGB_DEF, KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEF,               KRGB_DEF,
-            KRGB_PORTAL1,   KRGB_DEF,   KRGB_DEF,               KRGB_DEF,   KRGB_DEF,   KRGB_DEF, KRGB_DEF,             KRGB_DEF,               KRGB_FN,    KRGB_PORTAL2, KRGB_DEF, KRGB_DEF,       KRGB_DEF
-    ),
-    [1] = RGBLAYOUT(
-            KRGB_OFF, KRGB_MEDIA,   KRGB_MEDIA,   KRGB_MEDIA,   KRGB_MEDIA,   KRGB_DEF, KRGB_DEF, KRGB_DEF, KRGB_DEF, KRGB_DEF, KRGB_DEF,   KRGB_DEF, KRGB_DEF, KRGB_OFF, KRGB_DEF,
-            KRGB_OFF, KRGB_OFF,     KRGB_OFF,     KRGB_OFF,     KRGB_OFF,     KRGB_OFF,           KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF,   KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF,
-            KRGB_OFF, KRGB_OFF,     KRGB_VAL,     KRGB_OFF,     KRGB_OFF,     KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF,   KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_DEF,
-            KRGB_DEF, KRGB_OFF,     KRGB_VAL,     KRGB_OFF,     KRGB_OFF,     KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF,   KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_DEF,
-            KRGB_OFF, KRGB_OFF,     KRGB_DEF,     KRGB_OFF,     KRGB_OFF,     KRGB_OFF, KRGB_FN,  KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_DEV2,  KRGB_OFF, KRGB_OFF,           KRGB_OFF,
-            KRGB_OFF, KRGB_OFF,     KRGB_OFF,                   KRGB_OFF,     KRGB_OFF, KRGB_OFF, KRGB_OFF,           KRGB_DEF,             KRGB_FN,  KRGB_OFF, KRGB_DEF, KRGB_OFF, KRGB_DEF
-    ),
-    [2] = RGBLAYOUT(
-            KRGB_DANGER,    KRGB_DANGER,    KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_DEF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF,
-            KRGB_OFF,       KRGB_OFF,       KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF,           KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF,
-            KRGB_OFF,       KRGB_OFF,       KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF,
-            KRGB_OFF,       KRGB_OFF,       KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF,
-            KRGB_OFF,       KRGB_OFF,       KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_FN,  KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF,           KRGB_OFF,
-            KRGB_OFF,       KRGB_OFF,       KRGB_OFF,           KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF,           KRGB_OFF,           KRGB_FN,  KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF
-    )
+	[0] = RGBLAYOUT(
+			KRGB_DEF,       KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEV2,  KRGB_DEF, KRGB_DEF, KRGB_DEF,   KRGB_DEV1,  KRGB_DEV2,  KRGB_DEV2,  KRGB_DEF,     KRGB_DEF, KRGB_MISNAMED,
+			KRGB_DEF,       KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEF,             KRGB_DEF, KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEF,     KRGB_DEF, KRGB_DEF,
+			KRGB_DEF,       KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEF, KRGB_DEF, KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEF,     KRGB_DEF, KRGB_MISNAMED,
+			KRGB_MISNAMED,  KRGB_HOME,  KRGB_HOME,  KRGB_HOME,  KRGB_HOME,  KRGB_DEF,   KRGB_OFF, KRGB_DEF, KRGB_HOME,  KRGB_HOME,  KRGB_HOME,  KRGB_HOME,  KRGB_DEF,     KRGB_DEF, KRGB_MISNAMED,
+			KRGB_DEF,       KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_OFF, KRGB_DEF, KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEF,   KRGB_DEF,               KRGB_DEF,
+			KRGB_PORTAL1,   KRGB_DEF,   KRGB_DEF,               KRGB_DEF,   KRGB_DEF,   KRGB_DEF, KRGB_DEF,             KRGB_DEF,               KRGB_FN,    KRGB_PORTAL2, KRGB_DEF, KRGB_DEF,       KRGB_DEF
+	),
+	[1] = RGBLAYOUT(
+			KRGB_OFF, KRGB_MEDIA,   KRGB_MEDIA,   KRGB_MEDIA,   KRGB_MEDIA,   KRGB_DEF, KRGB_DEF, KRGB_DEF, KRGB_DEF, KRGB_DEF, KRGB_DEF,   KRGB_DEF, KRGB_DEF, KRGB_OFF, KRGB_DEF,
+			KRGB_OFF, KRGB_OFF,     KRGB_OFF,     KRGB_OFF,     KRGB_OFF,     KRGB_OFF,           KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF,   KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF,
+			KRGB_OFF, KRGB_OFF,     KRGB_VAL,     KRGB_OFF,     KRGB_OFF,     KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF,   KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_DEF,
+			KRGB_DEF, KRGB_OFF,     KRGB_VAL,     KRGB_OFF,     KRGB_OFF,     KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF,   KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_DEF,
+			KRGB_OFF, KRGB_OFF,     KRGB_DEF,     KRGB_OFF,     KRGB_OFF,     KRGB_OFF, KRGB_FN,  KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_DEV2,  KRGB_OFF, KRGB_OFF,           KRGB_OFF,
+			KRGB_OFF, KRGB_OFF,     KRGB_OFF,                   KRGB_OFF,     KRGB_OFF, KRGB_OFF, KRGB_OFF,           KRGB_DEF,             KRGB_FN,  KRGB_OFF, KRGB_DEF, KRGB_OFF, KRGB_DEF
+	),
+	[2] = RGBLAYOUT(
+			KRGB_DANGER,    KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_DANGER,    KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_DEV2,  KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF,
+			KRGB_OFF,       KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF,                 KRGB_OFF, KRGB_OFF, KRGB_OFF,   KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF,
+			KRGB_OFF,       KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF,       KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF,   KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF,
+			KRGB_OFF,       KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF,       KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF,   KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF,
+			KRGB_OFF,       KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF,       KRGB_FN,  KRGB_OFF, KRGB_OFF, KRGB_OFF,   KRGB_OFF, KRGB_OFF, KRGB_OFF,           KRGB_OFF,
+			KRGB_OFF,       KRGB_OFF, KRGB_OFF,           KRGB_OFF, KRGB_OFF,       KRGB_OFF, KRGB_OFF,           KRGB_OFF,             KRGB_FN,  KRGB_OFF, KRGB_OFF, KRGB_OFF, KRGB_OFF
+	)
 
 };
 
@@ -110,17 +110,17 @@ void keyboard_post_init_user(void) {
 
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (record -> event.pressed) {
-        switch (keycode) {
-            case M_LAMBDA:
-                SEND_STRING(" => ");
-                return false; break;
-            default:
-                break;
-        }
-    }
+	if (record -> event.pressed) {
+		switch (keycode) {
+			case M_LAMBDA:
+				SEND_STRING(" => ");
+				return false; break;
+			default:
+				break;
+		}
+	}
 
-    return true;
+	return true;
 }
 
 
