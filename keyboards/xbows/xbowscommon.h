d33bd89aeaae4b95b8fcd3cd6af0abe152be0a80
Alberto Bec <alberto.bec@microfocus.com>
2022-01-26 18:34:10 -0500

    Lighting rework

--- keyboards/xbows/xbowscommon.h
+++ keyboards/xbows/xbowscommon.h
@@ -2,6 +2,20 @@
 
 #include QMK_KEYBOARD_H
 
+#ifdef XBC_DEF_RGB_PIN
+const uint8_t _xbc_defRgbPin = XBC_DEF_RGB_PIN;
+#else
+const uint8_t _xbc_defRgbPin = 255;
+#endif
+
+// This is a cheat to not have to deal with dynamically allocating memory
+// Setting XBC_DEF_LAYER_MAX_LENGTH allows for a minor optimization
+#ifdef XBC_LAYER_MAX_LENGTH
+const uint8_t _xbc_layerArrayLength = XBC_DEF_LAYER_MAX_LENGTH;
+#else
+const uint8_t _xbc_layerArrayLength = MATRIX_ROWS * MATRIX_COLS;
+#endif
+
 typedef struct {
   uint8_t h;
   uint8_t s;
@@ -11,8 +25,8 @@ typedef struct {
   uint8_t b;
 } KeyColor_s;
 
-enum KeyColor_e {
-    KRGB_DEF = -1,
+typedef enum {
+    KRGB_DEF = 0,
     KRGB_OFF,
     KRGB_VAL,   // Special value for the keys that control brightness so they don't fully turn off
     KRGB_HOME,
@@ -25,9 +39,28 @@ enum KeyColor_e {
     KRGB_PORTAL2,
     KRGB_DANGER,
     KRGB_WHITE
-};
+} KeyColor;
 
-typedef enum KeyColor_e KeyColor;
+typedef struct {
+    uint8_t pin;
+    uint8_t h;
+    uint8_t s;
+} KeyColorSetHs;
+
+typedef struct {
+    uint8_t pin;
+    KeyColor keyColor;
+} KeyColorSetPreset;
+
+typedef struct {
+    uint8_t length;
+    KeyColorSetHs arr[_xbc_layerArrayLength];
+} KeyColorSetHsArray;
+
+typedef struct {
+    uint8_t length;
+    KeyColorSetPreset arr[_xbc_layerArrayLength];
+} KeyColorSetPresetArr;
 
 KeyColor_s _xbc_colors[] = {
     [KRGB_OFF] = 		{},
@@ -44,22 +77,29 @@ KeyColor_s _xbc_colors[] = {
     [KRGB_WHITE] = 		{0, 0, 255, 255, 255, 255}
 };
 
-size_t _xbc_layerMemSize = sizeof(KeyColor) * MATRIX_ROWS * MATRIX_COLS;
+#ifdef XBC_KRGB_VAL_MIN
+uint8_t _xbc_KRGB_VAL_min = XBC_KRGB_VAL_MIN;
+#else
 uint8_t _xbc_KRGB_VAL_min = 33;
+#endif
 
-int (*_xbc_keyindices_ptr)[MATRIX_COLS];
-const KeyColor (*_xbc_rgblayers_ptr)[MATRIX_ROWS][MATRIX_COLS];
+uint8_t (*_xbc_keyindices_ptr)[MATRIX_COLS];
+const KeyColorSetPresetArr (*_xbc_rgblayers_ptr)[_xbc_layerMaxArray];
 
-KeyColor _xbc_currLayer[MATRIX_ROWS][MATRIX_COLS];
+KeyColorSetPresetArr _xbc_currLayer;
+KeyColorSetHsArray _xbc_currStreamedLayer;
 int _xbc_currLayerIndex = -1;
 uint8_t _xbc_currHsVal = 0;
 
 
-void xbc_initialize_rgb_layers(int layerSize, const KeyColor rgblayers[layerSize][MATRIX_ROWS][MATRIX_COLS], int keyindices[MATRIX_ROWS][MATRIX_COLS]) {
+void xbc_initialize_rgb_layers(int layerSize, const KeyColorSetPresetArr rgblayers[layerSize], uint8_t keyindices[MATRIX_ROWS][MATRIX_COLS]) {
     _xbc_keyindices_ptr = keyindices;
     _xbc_rgblayers_ptr = rgblayers;
 }
 
+RGB _xbc_HsToRgb(uint8_t h, uint8_t s) {
+    return hsv_to_rgb((HSV){h, s, _xbc_currHsVal});
+}
 
 KeyColor_s xbc_getKeyColor(KeyColor kc) {
     KeyColor_s c = _xbc_colors[kc];
@@ -68,7 +108,7 @@ KeyColor_s xbc_getKeyColor(KeyColor kc) {
 
     // Update RGB if brightness value has changed, but not if it would set brightness control keys to 0
     if (_xbc_currHsVal != c.v && !(_xbc_currHsVal < _xbc_KRGB_VAL_min && kc == KRGB_VAL)) {
-        RGB rgb = hsv_to_rgb((HSV){c.h, c.s, _xbc_currHsVal});
+        RGB rgb = xbc_HsToRgb(c.h, c.s);
         c = (KeyColor_s){ c.h, c.s, _xbc_currHsVal, rgb.r, rgb.g, rgb.b };
         _xbc_colors[kc] = c;
     }
@@ -77,12 +117,24 @@ KeyColor_s xbc_getKeyColor(KeyColor kc) {
 }
 
 
-void xbc_set_colors(int li) {
+void _xbc_setDefaultColor(uint8_t h, uint8_t s) {
+
+}
+
+void xbc_setLayerColors(int li) {
     if (li != _xbc_currLayerIndex) {
         _xbc_currLayerIndex = li;
         memcpy_P(_xbc_currLayer, _xbc_rgblayers_ptr[li], _xbc_layerMemSize);
     }
     _xbc_currHsVal = rgb_matrix_get_val();
+    int si = 0;
+
+    if (_xbc_currLayer.arr[si].pin == _xbc_defRgbPin) {
+        KeyColor_s c = xbc_getKeyColor(_xbc_currLayer.arr[si].keyColor);
+
+        _xbc_setDefaultColor(c.h, c.s);
+        si++;
+    }
 
     for (int ri = 0; ri < MATRIX_ROWS; ri++) {
         for (int ci = 0; ci < MATRIX_COLS; ci++) {
