abf860a102de8f030294171a125101592fb520d3
c0d3m0nk3y <4998029+c0d3m0nky@users.noreply.github.com>
2026-07-27 22:22:04 -0400

    MO_NEXT for dual dedicated Fn keys any chain order

--- keyboards/xbows/knight_plus/keymaps/default/keymap.c
+++ keyboards/xbows/knight_plus/keymaps/default/keymap.c
@@ -17,6 +17,7 @@
 
 enum custom_keycodes {
     M_LAMBDA = QK_USER, // Types " => "
+    MO_NEXT,            // Momentary: activate the next-higher layer (additive/stacking)
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -42,24 +43,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GRV,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                   KC_6,     KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,
       KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,     KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,    KC_HOME,
       KC_LSFT,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_NO,      KC_H,     KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,    KC_ENT,     KC_END,
-      KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_NO,      KC_N,     KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,                KC_UP,
-      KC_LCTL,    KC_LGUI,    KC_LALT,                KC_SPC,     KC_LCTL,    KC_LSFT,    KC_SPC,               KC_RALT,                MO(1),      KC_RCTL,    KC_LEFT,    KC_DOWN,  KC_RGHT
+      KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       MO_NEXT,    KC_N,     KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,                KC_UP,
+      KC_LCTL,    KC_LGUI,    KC_LALT,                KC_SPC,     KC_LCTL,    KC_LSFT,    KC_SPC,               KC_RALT,                MO_NEXT,    KC_RCTL,    KC_LEFT,    KC_DOWN,  KC_RGHT
   ),
   [1] = LAYOUT(
       KC_TRNS,    KC_MPLY,    KC_VOLD,    KC_VOLU,    KC_MUTE,    KC_F17,     KC_F18,     KC_F19,   KC_F20,     KC_F21,     KC_F22,     KC_F23,     KC_F24,     KC_TRNS,    KC_PSCR,
       KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
       KC_TRNS,    KC_TRNS,    RM_VALU,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_PGUP,
       KC_CAPS,    KC_TRNS,    RM_VALD,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_PGDN,
-      KC_TRNS,    KC_TRNS,    KC_CALC,    KC_TRNS,    KC_TRNS,    KC_TRNS,    MO(2),      KC_TRNS,  KC_TRNS,    KC_TRNS,    M_LAMBDA,   KC_TRNS,    KC_TRNS,                KC_TRNS,
-      KC_TRNS,    KC_TRNS,    KC_TRNS,                KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,              KC_MENU,                MO(1),      KC_TRNS,    KC_WBAK,    KC_TRNS,  KC_WFWD
+      KC_TRNS,    KC_TRNS,    KC_CALC,    KC_TRNS,    KC_TRNS,    KC_TRNS,    MO_NEXT,    KC_TRNS,  KC_TRNS,    KC_TRNS,    M_LAMBDA,   KC_TRNS,    KC_TRNS,                KC_TRNS,
+      KC_TRNS,    KC_TRNS,    KC_TRNS,                KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,              KC_MENU,                MO_NEXT,    KC_TRNS,    KC_WBAK,    KC_TRNS,  KC_WFWD
   ),
   [2] = LAYOUT(// Hardware layer
       QK_BOOT,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      EE_CLR,     KC_NO,      KC_NO,    KC_NO,      DB_TOGG,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
       KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,                  KC_NO,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
       KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
       KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
-      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      MO(2),      KC_NO,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,                  KC_NO,
-      KC_NO,      KC_NO,      KC_NO,                  KC_NO,      KC_NO,      KC_NO,      KC_NO,                KC_NO,                  MO(1),      KC_NO,      KC_NO,      KC_NO,    KC_NO
+      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      MO_NEXT,    KC_NO,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,                  KC_NO,
+      KC_NO,      KC_NO,      KC_NO,                  KC_NO,      KC_NO,      KC_NO,      KC_NO,                KC_NO,                  MO_NEXT,    KC_NO,      KC_NO,      KC_NO,    KC_NO
   )//                                                                         ▲▲▲▲▲▲▲                                                               |||||||
 };
 
@@ -70,6 +71,23 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 SEND_STRING(" => ");
             }
             return false;
+        case MO_NEXT: {
+            // Momentary "layer up": stack onto whatever layer is currently highest
+            // so either Fn key can start the climb and the other continues it.
+            uint8_t max_layer = (uint8_t)(sizeof(keymaps) / sizeof(keymaps[0]) - 1);
+            if (record->event.pressed) {
+                uint8_t target = get_highest_layer(layer_state) + 1;
+                if (target <= max_layer) {
+                    layer_on(target);
+                }
+            } else {
+                uint8_t current = get_highest_layer(layer_state);
+                if (current > 0) {
+                    layer_off(current);
+                }
+            }
+            return false;
+        }
     }
     return true;
 }
